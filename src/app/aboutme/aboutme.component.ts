31525e32bb8543d1bbac1df0f4853a8c1f40f6d4
MUHAMAD GALIH <100691045+muhgalihhh@users.noreply.github.com>
2024-10-31 01:05:13 +0700

    ubah nim shift

--- src/app/aboutme/aboutme.component.ts
+++ src/app/aboutme/aboutme.component.ts
@@ -8,9 +8,9 @@ import { Component } from '@angular/core';
 export class AboutmeComponent {
   name: string = 'Muhamad Galih';
   bio: string = 'Aspiring graphic designer.';
-  nim: string = '123456';
-  shiftLama: string = 'Morning';
-  shiftBaru: string = 'Afternoon';
+  nim: string = 'H1D022052';
+  shiftLama: string = 'B';
+  shiftBaru: string = 'E';
   contact: string = '081234567890';
   email: string = 'galih@example.com';
   address: string = 'Kota Banjar, Indonesia';
